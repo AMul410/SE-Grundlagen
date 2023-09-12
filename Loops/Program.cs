7f962f6ee61a1d355eb69aa7fce000ca615e29ca
AMulh410 <140390970+AMul410@users.noreply.github.com>
2023-09-12 11:14:42 +0200

    Initialise

---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Loops-Demo");
+int iterCount = 10;
+
+while (iterCount > 0)
+{
+  Console.WriteLine("WHILE");
+  iterCount = iterCount - 1;
+  //iterCount--; not --iterCount (Precremente operator)!!!!! Warning!!!!
+}
+
+iterCount = 10;
+do
+{
+  Console.WriteLine("DO-WHILE");
+  iterCount--;
+} while (iterCount > 0);
+
+
+Console.WriteLine("FOR-LOOP");
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine("FOR");
+}
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Welcome to Calc 3000");
+bool checkParse1 = false;
+bool checkParse2 = false;
+int num1 = 0;
+int num2 = 0;
+
+while (!checkParse1 || !checkParse2)
+{
+  Console.WriteLine("Pls provide the 1st number");
+  var input1 = Console.ReadLine();
+
+  Console.WriteLine("Pls provide 2nd number");
+  var input2 = Console.ReadLine();
+
+
+  // Konvertierung von Text zu Zahlen
+  checkParse1 = int.TryParse(input1, out num1);
+  checkParse2 = int.TryParse(input2, out num2);
+
+}
+var result = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {result}");
+
+
+
+
+// "Var ist nicht kombinierbar mit Datentypenangaben
+// Entweder (Var) oder Datentyp eg. String, int, float.
