e33895ab5a3e66c3ddb29015e85bf6e770995c3d
AMulh410 <140390970+AMul410@users.noreply.github.com>
2023-09-26 11:57:07 +0200

    Added Beginning of Cats

---
+++ GrundlagenOOP/Cat.cs
@@ -0,0 +1,31 @@
+﻿namespace GrundlagenOOP;
+
+public class Cat
+{
+  public string Name;
+  public int Age;
+  public string Color;
+
+  public Cat(string name, int age, string color) 
+  {
+    this.Name = name;
+    this.Age = age;
+    this.Color = color;
+  }
+
+  // Konstruktor -> Erstellt eine neue Instanz der Klasse Cat
+  // -> Nicht für Static Classes
+  public void Miau()
+  {
+    Console.WriteLine("Miau!");
+  }
+  public static void Schnurren()
+  {
+    Console.WriteLine("purrrrrrrr......");
+  }
+  public override string ToString()
+  {
+    // $"Text{x}" -> String-Interpolation
+    return $"name: {this.Name}\nAge: {this.Age}\nColor: {this.Color}";
+  }
+}
---
+++ GrundlagenOOP/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GrundlagenOOP;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, World!"); // Statische-Klasse
+    Random rng = new Random(); // Instabzierbare-Klasse
+    // Cat myCat = new Cat();
+    int x;
+    Cat y; // Cat is our own type
+    // myCat.Miau();
+    // myCat.Name = "Vanessa";
+    // myCat.Age = 10;
+    // myCat.Color = "Silver-Tabby";
+    // Cat myCat2 = new Cat();
+    // myCat2.Color = "Brown";
+    // myCat2.Name = "Kaja";
+    // myCat2.Age = 5;
+    // Console.WriteLine(myCat);
+    // Console.WriteLine(myCat2);
+    // myCat2.Name = "JK";
+    // Console.WriteLine(myCat2);
+
+    Cat jerry = new Cat("jerry", 8, "Black");
+    Console.WriteLine(jerry);
+    jerry.Age += 1;
+
+    Console.WriteLine(jerry);
+    
+    jerry.Miau();
+    Cat.Schnurren();
+    
+
+    Console.ReadLine();
+
+
+  }
+}
