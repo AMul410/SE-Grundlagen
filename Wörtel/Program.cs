76b57a75eacbe00b9775b6970c6e2a8fa4af0696
AMulh410 <140390970+AMul410@users.noreply.github.com>
2023-09-27 14:49:58 +0200

    Start des Wörtel (German Worldle)

---
+++ Wörtel/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Programmstart: 
+ * 
+ * Definiere solutionWord als das zu erratende Wort fest
+ * 
+ * Schleife:
+ *    Definiere guess und speicher das vom User eingegebene Wort
+ *    Schleife für jeden Buchstaben in guess:
+ *        Schleife für jeden Buchstaben im solutionWord:
+ *            Prüfe Buchstabe aus guess gleich Buchstabe in solutionWord:
+ *                Wahr:
+ *                
+ */
+
+var solutionWord = "HOTEL";
+
+string guess;
+
+while (true)
+{
+  Console.WriteLine("Errate das 5-Stellige Wort");
+  guess = Console.ReadLine();
+  if (guess.Length != 5) continue;
+
+  // foreach (char c in guess)
+  // {
+  //  if (solutionWord.Contains(c));
+  //    Console.BackgroundColor = ConsoleColor.Yellow;
+  //  if (/* ... */)
+  //    Console.BackgroundColor = ConsoleColor.Green;
+  //    Console.WriteLine($"{c}");
+  // }
+
+
+
+  // Gedankenspiel
+
+  for(int i = 0; i < guess.Length; i++)
+  {
+    if (solutionWord.Contains(guess[i]))
+      Console.BackgroundColor = ConsoleColor.Yellow;
+    if (solutionWord[i] == guess[i])
+      Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{guess[i]}");
+  }
+
+  // int i = 0;
+  // while (i < guess.Length)
+  // {
+  //   var c = guess[i];
+  //   // dein code ..
+  //  i ++;
+  // }
+}
